ac9e69cb4c480d39def952ed4f4792c59391f9af
taher.galal90@gmail.com <taher.galal90@gmail.com@3d59e65c-d65c-cdb3-7de1-4d273c007014>
2011-11-19 19:56:53 +0000

    created State with the following attributes cell, orintation, holding_gold,path

---
+++ trunk/AIProject/src/State.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 19, 2011
+
+@author: taher
+'''
+class State(object):
+    
+    def __init__(self, initial_balance=0):
+        self.orintation = "f"
+        self.cell = tuple()
+        self.holding_gold = False
+        self.path = tuple() 
+
+
+first_instance =State()
+print first_instance.orintation
+
