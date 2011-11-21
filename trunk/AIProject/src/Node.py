66ea0ba72548b44d80ee9998a0814c5ea6a1e217
taher.galal90 <taher.galal90@3d59e65c-d65c-cdb3-7de1-4d273c007014>
2011-11-21 21:23:41 +0000

    Created node object

---
+++ trunk/AIProject/src/Node.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 21, 2011
+
+@author: taher
+'''
+import State
+
+class MyClass(object):
+   
+
+
+    def __init__(self, pathCost):
+        self.State =State()
+        self.pathCost = 0
+        self.listOfActions = []
+        
