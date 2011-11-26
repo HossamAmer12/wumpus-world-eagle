c48b902899e8d260f9db1c541f95eaa20b528bd5
mhmgad@gmail.com <mhmgad@gmail.com@3d59e65c-d65c-cdb3-7de1-4d273c007014>
2011-11-26 13:27:44 +0000

    remove position list (path list)

--- trunk/AIProjectV2/src/State.py
+++ trunk/AIProjectV2/src/State.py
@@ -6,13 +6,10 @@ Created on Nov 19, 2011
 class State:
     pass
     
-    def __init__(self, orintation = 'N', position = (0,0), holdingGold = False, killedWampus = False, path = []):
+    def __init__(self, orintation = 'N', position = (0,0), holdingGold = False, killedWampus = False):
         self.orintation = orintation
         self.position = position
         self.holdingGold = holdingGold
-        self.path =[]
-        self.path.extend(path)
-        self.path.append(position)
         self.killedWampus = killedWampus
 
     def __str__(self):
